3057afd7be1b7dbb246043b632a2cade3c83d291
sergiomerces <sergio.merces@gmail.com>
2023-02-26 00:18:24 -0300

    operadores e funções matemáticas

---
+++ operadores/operadores.js
@@ -0,0 +1,36 @@
+/*
+Operadores matemáticos
+
++   soma
+-   subtração
+*   multiplicação
+/   divisão
+**  exponenciaçao
+%   módulo 
+
+let a = 5;
+let b = 2;
+let c = 0;
+
+Funções Matemáticas
+
+Math.abs(num)   retorna o valor absoluto, se o número for negativo retorna poasitivo, se for ppositivo retorna ele mesmo
+
+Math.ceil(num)  arredondamento para cima
+
+Math.floor(num) arredondamentoto para baixo
+
+Math.pow(base, exp) retorna a base elevada ao expoente
+
+Math.random() retorna um número randômico entre o e 1 com várias casas decimais
+
+Math.round()   arredonda o valor para o inteiro mais próximo, acima de 5 para cima, abaixo, para baixo
+
+Math.sqrt(num)
+
+*/
+
+let num = Math.sqrt(144);
+console.log(num);
+
+
