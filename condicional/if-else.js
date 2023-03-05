594ea7143181fc04d419c9f0b5494e324d8bd390
sergiomerces <sergio.merces@gmail.com>
2023-03-05 18:43:08 -0300

    condicional if...else e operadores relacionais

---
+++ condicional/if-else.js
@@ -0,0 +1,44 @@
+/*
+Estrutura condicional if ... else
+
+Ela funciona a partir de uma condição, se esta for verdadeira será executado um bloco de código; caso seja falsa poderá ser executado outro bloco de código.
+
+Sintaxe:
+
+if(condicao) {
+    bloco comando verdadeiro
+} else  {
+    bloco comando falso
+}
+
+Aninhamento de if's:
+
+if(condicao1) {
+    bloco comando condição1
+} else if(condicao2)  {
+    bloco comando condição2
+} else {
+    bloco padrão
+}
+*/
+
+/*
+
+Operadores relacionais
+
+== igual, retorna verdadeiro se os dados forem do mesmo valor
+
+=== idêntico, retorna verdadeiro se os dados e os tipos forem iguais
+
+!= diferente, retorna verdadeiro se dos dados forem diferentes
+
+!== estritamente diferente, retorna verdadeiro se os valores e tipos forem diferentes
+
+> maior
+
+< menor
+
+>= maior ou igual
+
+<= menor ou igual
+*/
