3570962598d023ca7e20e98d4de7cbe3b5f2d055
sergiomerces <sergio.merces@gmail.com>
2023-03-08 17:54:14 -0300

    média escolar

--- media-escolar/script.js
+++ media-escolar/script.js
@@ -1,5 +1,23 @@
 function exibirMedia() {
-    alert('Função funcionando!')
+    let nome = document.getElementById('nome').value;
+    let nota1 = Number(document.getElementById('nota1').value);
+    let nota2 = Number(document.getElementById('nota2').value);
+    let outMedia = document.getElementById('outMedia');
+    let outSituacao = document.getElementById('outSituacao');
+
+    let media = (nota1 + nota2) / 2;
+
+    if(media >= 7.0) {
+        outMedia.textContent = `Médias das notas: ${media.toFixed(1)}`;
+        outSituacao.textContent = `Parabéns ${nome}! Você foi Aprovado(a)!`;
+        outMedia.style.color = 'blue';
+        outSituacao.style.color = 'blue';
+    } else {
+        outMedia.textContent = `Médias das notas: ${media.toFixed(1)}`;
+        outSituacao.textContent = `Lamentamos ${nome}! Você foi Reprovado(a)!`;
+        outMedia.style.color = 'red';
+        outSituacao.style.color = 'red';
+    }
 }
 
 const botao = document.getElementById('botao');
