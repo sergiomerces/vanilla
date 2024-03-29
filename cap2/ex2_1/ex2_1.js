c4c31a6bc451240a541516a0476b5c83b2d75650
sergiomerces <sergio.merces@gmail.com>
2024-03-29 04:33:54 -0300

    notas do programa dizer olá

--- cap2/ex2_1/ex2_1.js
+++ cap2/ex2_1/ex2_1.js
@@ -5,3 +5,11 @@ function dizerOla(){
 
 const btnMostrar = document.getElementById('mostrar');
 btnMostrar.onclick = dizerOla;
+
+/*
+Ao escrever uma função JScript
+1. capturar o conteúdo do elemento numa variável
+2. Aaravés do método atribuir um valor ao elemento resposta
+3. a captura do botão deve ser de escopo global (fora daschaves)
+4. Para chamar a função atribuir seu nome a variavel e um evento
+*/
