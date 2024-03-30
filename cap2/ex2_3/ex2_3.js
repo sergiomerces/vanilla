b09d3911fb0ff74cff2a2b4a9c1a297ebbc25016
sergiomerces <sergio.merces@gmail.com>
2024-03-30 01:24:22 -0300

    app reenda de veiculo

---
+++ cap2/ex2_3/ex2_3.js
@@ -0,0 +1,18 @@
+function mostrarPromocao(){
+    const inVeiculo = document.getElementById('inVeiculo');
+    const inPreco = document.getElementById('inPreco');
+    const outVeiculo = document.getElementById('outVeiculo');
+    const outEntrada = document.getElementById('outEntrada');
+    const outParcelas = document.getElementById('outParcelas');
+
+    const preco = Number(inPreco.value);
+    const entrada = preco / 2;
+    const parcelas = (preco - entrada) / 12;
+
+    outVeiculo.textContent = inVeiculo.value;
+    outEntrada.textContent = `Entrada de R$ ${entrada.toFixed(2)}`;
+    outParcelas.textContent = `12x de R$ ${parcelas.toFixed(2)}`;
+}
+
+const btnPromocao = document.getElementById('btnPromocao');
+btnPromocao.addEventListener('click', mostrarPromocao);
