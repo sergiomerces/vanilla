185657e485b470c151a3ce0c0497ac96e8e6ff0e
sergiomerces <sergio.merces@gmail.com>
2024-03-31 20:16:53 -0300

    finalizando o app restaurante

--- cap2/ex2_4/ex2_4.js
+++ cap2/ex2_4/ex2_4.js
@@ -1,9 +1,14 @@
 function calcularPreco(){
     const inPreco = document.getElementById('inPreco');
-    const preco = Number(inPreco.value);
     const inConsumo = document.getElementById('inConsumo');
-    const consumo = Number(inConsumo.value);
     const outValor = document.getElementById('outValor');
+    
+    const preco = Number(inPreco.value);
+    const consumo = Number(inConsumo.value);
+    const totalAPagar = (preco * consumo) / 1000;
 
+    outValor.textContent = `Total a pagar R$ ${totalAPagar.toFixed(2)}`;
 }
 
+const btnCalcular = document.getElementById('btnCalcular');
+btnCalcular.addEventListener('click', calcularPreco);
