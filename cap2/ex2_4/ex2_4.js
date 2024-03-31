9ad4e72660053224855200cd887721b6c731e243
sergiomerces <sergio.merces@gmail.com>
2024-03-31 19:36:19 -0300

    app restaurante

---
+++ cap2/ex2_4/ex2_4.js
@@ -0,0 +1,9 @@
+function calcularPreco(){
+    const inPreco = document.getElementById('inPreco');
+    const preco = Number(inPreco.value);
+    const inConsumo = document.getElementById('inConsumo');
+    const consumo = Number(inConsumo.value);
+    const outValor = document.getElementById('outValor');
+
+}
+
