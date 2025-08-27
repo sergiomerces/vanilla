c668ab2b90e8cb2441bec6b56ad58385c5e38bdf
sergiomerces <sergio.merces@gmail.com>
2025-08-27 01:44:58 -0300

    exercício validação co if

--- cap3/ex_3-6a/ex_3-6a.js
+++ cap3/ex_3-6a/ex_3-6a.js
@@ -1,1 +1,24 @@
-console.log(hello);
+const inHora = document.getElementById('inHora');
+const outResposta = document.getElementById('outResposta');
+
+function exibirHorario() {
+
+    let hora = Number(inHora.value);
+
+    if(!hora || isNaN(hora)) {
+        alert('Informe a hora no Brasil corretamente!');
+        inHora.focus();
+        return;
+    }
+
+    let horaFranca = hora + 5;
+
+    if(hora > 24)
+        horaFranca = horaFranca - 24;
+
+    outResposta.textContent = `Hora na França: ${horaFranca.toFixed(2)}`;
+
+}
+
+const btExibir = document.getElementById('btExibir');
+btExibir.addEventListener('click', exibirHorario);
